458c10db64469f1b25a3fd0e87765e9d5c50a72e
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-02-23 12:28:32 +0100

    test training

--- bodynavigation/advanced_segmentation/sdf_train_eval_test.py
+++ bodynavigation/advanced_segmentation/sdf_train_eval_test.py
@@ -1,13 +1,23 @@
 import unittest
 import pytest
 from bodynavigation.advanced_segmentation import sdf_seg_pipeline
+from pathlib import Path
 
 
-def test_sdf_training():
+def test_sdf_prepare_data():
+    sdf_type = 'surface'
+    imshape = 256
     sdf_seg_pipeline.prepare_data(
         n_data=1,
         # skip_h5=True,
-        sdf_type='surface',
+        imshape=imshape,
+        sdf_type=sdf_type,
         filename_prefix='testfile_', # prevent rewriting the files during test
     )
+    assert Path(f"testfile_sdf_{sdf_type}{imshape}.h5").exists()
+
+def test_sdf_training():
+    from bodynavigation.advanced_segmentation import sdf_unet256
+    sdf_unet256
+    assert True
 
