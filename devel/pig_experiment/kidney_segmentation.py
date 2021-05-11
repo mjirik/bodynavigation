c59c9de0ca7a8d5b79720d5ea984ef0de15a1680
Miroslav Jiřík <miroslav.jirik@gmail.com>
2021-05-11 22:27:02 +0200

    balance added, graphs prepare

--- devel/pig_experiment/kidney_segmentation.py
+++ devel/pig_experiment/kidney_segmentation.py
@@ -20,6 +20,7 @@ import sed3
 from loguru import logger
 from sklearn.svm import SVC
 import bodynavigation
+import imma
 # logger.disable("io3d")
 logger.remove()
 logger.add(sys.stderr, level='INFO')
@@ -35,17 +36,20 @@ def externfv(data3d, voxelsize_mm):        # scale
     f1 = scipy.ndimage.filters.gaussian_filter(data3d, sigma=1)- f0 #hranový.
     ss = bodynavigation.body_navigation.BodyNavigation(data3d, voxelsize_mm)
     f3 = ss.dist_sagittal()
-    #f4 = ss.dist_coronal()
-    #f5= ss.dist_to_diaphragm_axial() # distance from heart
+    f4 = ss.dist_coronal()
+    f5= ss.dist_to_diaphragm_axial() # distance from heart
     #dist saggital, coronall
     fv = np.concatenate([
-        f0.reshape(-1, 1), f1.reshape(-1, 1), f3.reshape(-1, 1), #f4.reshape(-1, 1), f5.reshape(-1, 1)
+        f0.reshape(-1, 1), f1.reshape(-1, 1), f3.reshape(-1, 1),
+        f4.reshape(-1, 1),
+        f5.reshape(-1, 1)
     ], 1)
     np.save('Abc.npy', fv)
     #np.load
     return fv
 # Trainer function
-def train(patients_IDs, show=False): # a - first CT patient, b - last CT patient. We need existence of all patients between this 2 numbers.
+
+def train(patients_IDs, show=False, organ_label="RightKidney", do_balance=True): # a - first CT patient, b - last CT patient. We need existence of all patients between this 2 numbers.
     ol = imtools.trainer3d.Trainer3D()
     ol.working_voxelsize_mm = [3, 3, 3]
     # select feature function
@@ -62,27 +66,37 @@ def train(patients_IDs, show=False): # a - first CT patient, b - last CT patient
         i = z
         datap = io3d.datasets.read_dataset("pilsen_pigs", 'data3d', i)
         datap['data3d'][datap['data3d']<-200]=-200
-        datap_liver = io3d.datasets.read_dataset("pilsen_pigs", 'RightKidney', i)
+        datap_liver = io3d.datasets.read_dataset("pilsen_pigs", organ_label, i)
         # print('datap', datap)
 
         # print('datap_liver', datap_liver)
         z=z+3 # mezi 26 a 30 4 patieny
-    mask_kidney = datap_liver['data3d'] > 0
+        mask_kidney = datap_liver['data3d'] > 0
 
-    #sed3.ipy_show_slices(mask_kidney)
-    if show:
-        ad=sed3.sed3(datap['data3d'], contour=mask_kidney)
-        ad.set_window(40,400)
-        ad.show()
-    Data3D = datap["data3d"]
-    MASK = (mask_kidney).astype(np.uint8)
-    Data3D.dtype
-    ol.add_train_data(Data3D, MASK, voxelsize_mm=datap["voxelsize_mm"])
+        #sed3.ipy_show_slices(mask_kidney)
+        if show:
+            ad=sed3.sed3(datap['data3d'], contour=mask_kidney)
+            ad.set_window(40,400)
+            ad.show()
+        data3d = datap["data3d"]
+        MASK = (mask_kidney).astype(np.uint8)
+        logger.debug(np.unique(MASK, return_counts=True))
+
+        nth = 1 # save all voxels
+        ol.add_train_data(data3d, MASK, voxelsize_mm=datap["voxelsize_mm"], nth=nth)
+        if do_balance:
+            data, target = balance_data(ol.data, ol.target)
+            ol.data = data
+            ol.target = target
+
+    np.save('data.npy', ol.data)
+    np.save('target.npy', ol.target)
     ol.fit()
     if show:
-        plt.hist(Data3D.reshape(-1, 1), bins=30)
+        plt.hist(data3d.reshape(-1, 1), bins=30)
         plt
     return ol
+
 def klasification(test_patient, ol, first_slice=50, last_slice=900, show=True):
     # one = list(imtools.datasets.sliver_reader("*000.mhd", read_seg=True))[0]
     # numeric_label, vs_mm, oname, orig_data, rname, ref_data = one
@@ -99,3 +113,47 @@ def klasification(test_patient, ol, first_slice=50, last_slice=900, show=True):
     #plt.imshow(crop[125], cmap='gray', clim=[-200, 200])
     #plt.contour(fit[125])
     #plt.show()
+
+
+def balance_data(X, y):
+    import sklearn.utils
+
+    cls, cts = np.unique(y, return_counts=True)
+    mn = np.min(cts)
+    balanced_data = []
+    balanced_target = []
+    for cl in cls:
+        x_i = sklearn.utils.resample(X[(y == cl).flatten()], n_samples=mn)
+        y_i = np.array([cl] * len(x_i))
+        balanced_target.append(y_i)
+        balanced_data.append(x_i)
+
+    balanced_data = np.concatenate(balanced_data, axis=0)
+    balanced_target = np.concatenate(balanced_target, axis=0)
+    return balanced_data, balanced_target
+
+
+def add_train_data(self, data3d, segmentation, voxelsize_mm, nth=50):
+    data3dr = imma.image_manipulation.resize_to_mm(data3d, voxelsize_mm, self.working_voxelsize_mm)
+    segmentationr = imma.image_manipulation.resize_to_shape(segmentation, data3dr.shape)
+    _add_to_training_data(self, data3dr, segmentationr, nth)
+    # self._add_to_training_data(data3dr, segmentationr, nth)
+
+
+def _fv(self, data3dr):
+    return self.feature_function(data3dr, self.working_voxelsize_mm)
+
+
+def _add_to_training_data(self, data3dr, segmentationr, nth=50):
+    fv = self._fv(data3dr)
+    data = fv[::nth]
+    target = np.reshape(segmentationr, [-1, 1])[::nth]
+    #         print "shape ", data.shape, "  ", target.shape
+
+    if self.data is None:
+        self.data = data
+        self.target = target
+    else:
+        self.data = np.concatenate([self.data, data], 0)
+        self.target = np.concatenate([self.target, target], 0)
+        # self.cl.fit(data, target)
--- devel/pig_experiment/test_pig_experiment.py
+++ devel/pig_experiment/test_pig_experiment.py
@@ -1,7 +1,16 @@
 import pytest
 import io3d
 import sed3
+from loguru import logger
+import numpy as np
+import pandas as pd
 from . import kidney_segmentation
+
+# # On my harddrive, the data3d is stored in .mhd file so i had to change the dataset path pattern
+# io3d.datasets.DATASET_PATH_STRUCTURE["pilsen_pigs"]={
+#     '_': 'medical/orig/pilsen_pigs/Tx{id:03d}D_{subtype}/MASKS_DICOM/{data_type}/', # masks
+#     'data3d': 'medical/orig/pilsen_pigs/Tx{id:03d}D_{subtype}/PATIENT_DICOM/Tx{id:03d}D_{subtype}.mhd' # intensity data
+# }
 
 def test_trenovani():
     kidney_segmentation.vytiskni("ahoj")
@@ -9,16 +18,26 @@ def test_trenovani():
     # ed = sed3.sed3(dp['data3d'])
     # ed.show()
     sed3.show_slices(dp['data3d'], slice_number=24)
+
+
 def test_trainer():
     ol=kidney_segmentation.train([30, 30])
     kidney_segmentation.klasification(30, ol)
+
+
 def test_train_and_save():
-    ol = kidney_segmentation.train([30], show=True)
+    ol = kidney_segmentation.train([27], show=True, organ_label="right_kidney")
     import joblib
     joblib.dump(ol, 'ol.joblib')
+
 
 def test_train_load():
     import joblib
     ol=joblib.load('ol.joblib')
     print(ol)
-    kidney_segmentation.klasification(30, ol)
+    kidney_segmentation.klasification(29, ol)
+
+
+def test_check_path_to_pilsen_pigs():
+    pth = io3d.datasets.get_dataset_path("pilsen_pigs", 'data3d', 27)
+    logger.debug(pth)
